24d8b9242773545cb789feec1afc94e76fb5de1d
reditall <reditall>
2024-04-19 16:47:57 +0200

    add first page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let menuIcon = document.querySelector('#menu-icon');
+let naBar = document.querySelector('.navbar');
+let navLinks = document.querySelector('header nav a');
+
+
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    naBar.classList.toggle('active');
+    console.log(naBar.style);
+}
