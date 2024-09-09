2a75969eea9d3c8753ac9a4c0736b5b2a8cd90c5
Achraf Ayar <achrafayar00@gmail.com>
2024-09-09 11:51:42 +0100

    feat(blog): add blog

--- js/main.js
+++ js/main.js
@@ -1,17 +1,69 @@
-let menuIcon = document.querySelector('#menu-icon');
-let naBar = document.querySelector('.navbar');
-let navLinks = document.querySelectorAll('header nav a');  // Updated to select all nav links
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    naBar.classList.toggle('active');
-    console.log(naBar.style);
-}
-
-// Optional: Add an event listener to close the menu when a link is clicked
-navLinks.forEach(link => {
-    link.onclick = () => {
-        menuIcon.classList.remove('bx-x');
-        naBar.classList.remove('active');
-    };
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const instance = document.querySelector('.swiper.blog-slider');
+    const box = instance.querySelector('.swiper-wrapper.blog-container');
+    const prevArrow = instance.querySelector('.arrow-prev');
+    const nextArrow = instance.querySelector('.arrow-next');
+  
+    let isDragging = false;
+    let startX = 0;
+    let scrollLeft = 0;
+    const maxScrollWidth = box.scrollWidth - box.clientWidth;
+  
+    // Arrow click functionality
+    nextArrow.addEventListener('click', function () {
+      const x = (box.clientWidth / 2) + box.scrollLeft - 10;
+      box.scrollTo({
+        left: x,
+        behavior: 'smooth'
+      });
+    });
+  
+    prevArrow.addEventListener('click', function () {
+      const x = box.scrollLeft - (box.clientWidth / 2) + 10;
+      box.scrollTo({
+        left: x,
+        behavior: 'smooth'
+      });
+    });
+  
+    // Dragging functionality
+    box.addEventListener('mousedown', (e) => {
+      isDragging = true;
+      startX = e.pageX - box.offsetLeft;
+      scrollLeft = box.scrollLeft;
+      box.classList.add('dragging');
+    });
+  
+    box.addEventListener('mousemove', (e) => {
+      if (!isDragging) return;
+      const x = e.pageX - box.offsetLeft;
+      const walk = (x - startX) * 2; // Multiplied for more scroll speed
+      box.scrollLeft = scrollLeft - walk;
+    });
+  
+    box.addEventListener('mouseup', () => {
+      isDragging = false;
+      box.classList.remove('dragging');
+    });
+  
+    box.addEventListener('mouseleave', () => {
+      isDragging = false;
+      box.classList.remove('dragging');
+    });
+  
+    // Toggle arrows based on scroll position
+    function toggleArrows() {
+      if (box.scrollLeft === 0) {
+        prevArrow.classList.add('disabled');
+      } else if (box.scrollLeft >= maxScrollWidth) {
+        nextArrow.classList.add('disabled');
+      } else {
+        prevArrow.classList.remove('disabled');
+        nextArrow.classList.remove('disabled');
+      }
+    }
+  
+    box.addEventListener('scroll', toggleArrows);
+    toggleArrows();
+  });
+  
